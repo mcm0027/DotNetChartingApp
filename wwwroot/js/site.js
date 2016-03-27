d1684342b45493091b23240a76774b6750c53358
Matt Morris <mcm0027@yahoo.com>
2016-03-27 17:48:42 -0500

    add angular and bootstrap ui dependencies

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,2 +1,26 @@
 ﻿
 
+(function (angular) {
+    'use strict';
+    angular
+        .module('myApp', [
+            'ngAnimate',
+            'ngTouch',
+            'ui.bootstrap'
+        ])
+    //.config(['$routeProvider', function ($routeProvider) {
+    //    $routeProvider.otherwise({ redirectTo: '/' });
+
+    //}])
+    .controller('mainController', [
+        mainController])
+
+    .factory('projectsService', [
+        projectsService
+    ]);
+
+    function mainController() {
+
+    }
+
+})(window.angular);
