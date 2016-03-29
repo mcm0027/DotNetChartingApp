c9d1a788ac4277c3b880d25e4907e60cd099dda0
Matt Morris <mcm0027@yahoo.com>
2016-03-28 22:54:37 -0500

    create post method for occupancy data

--- Controllers/Api/OccupancyController.cs
+++ Controllers/Api/OccupancyController.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using ChartingProject.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ChartingProject.Controllers.Api
 {
+    [Route("api/occupancy")]
     public class OccupancyController : Controller
     {
-
-
-        [HttpGet("api/occupancy")]
+        [HttpGet("")]
         public JsonResult Get()
         {
             return Json(new { name = "Matt" });
+        }
+
+        [HttpPost("")]
+        public JsonResult Post([FromBody]Occupancy newOccupancy)
+        {
+            return Json(true);
         }
     }
 }
